6fa2fd75c0e7da077b8310b2d27d401c2eafc352
itstumpa <tumpadas1fiverr@gmail.com>
2026-05-16 20:29:21 +0600

    update sendEmail.ts

--- itstumpa/backend/src/utils/sendEmail.ts
+++ itstumpa/backend/src/utils/sendEmail.ts
@@ -32,13 +32,6 @@
 // src/utils/sendEmail.ts
 import { Resend } from "resend";
 
-interface EmailOptions {
-  to: string;
-  subject: string;
-  html: string;
-}
-
-
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 interface EmailOptions {
