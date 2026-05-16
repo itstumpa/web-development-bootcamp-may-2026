50ef9a99d78fd65b6311261e2877671f20ba0c75
itstumpa <tumpadas1fiverr@gmail.com>
2026-05-16 18:54:38 +0600

    update socket.ts

--- itstumpa/frontend/src/lib/socket.ts
+++ itstumpa/frontend/src/lib/socket.ts
@@ -1,12 +1,14 @@
 import { io, Socket } from "socket.io-client";
 
+const SOCKET_URL = process.env.NEXT_PUBLIC_BACKEND_URL?.replace("/api/v1", "") ?? "";
+
 let socket: Socket | null = null;
 
 export const connectSocket = (): Socket => {
   if (!socket) {
-    socket = io(process.env.NEXT_PUBLIC_BACKEND_URL!, {
+    socket = io(SOCKET_URL, {
       withCredentials: true,
-      transports: ["websocket"],
+      transports: ["websocket", "polling"],
     });
   }
   return socket;
